c0d5dbd476729b85c711e77b634d60ad53914ebc
PurpleOrangeGames <tiago.carvalho@purpleorangegames.com>
2018-05-31 22:16:14 -0300

    Add files via upload

---
+++ AnimalRaiders/AnimalRaiders.cs
@@ -0,0 +1,36 @@
+﻿using Harmony;
+using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+
+namespace AnimalRaiders
+{
+    [StaticConstructorOnStartup]
+    class AnimalRaiders : Mod
+    {
+        public AnimalRaiders(ModContentPack content) : base(content)
+        {
+            var harmony = HarmonyInstance.Create("net.pog.rimworld.mod.animalraiders");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+
+    [HarmonyPatch(typeof(LordToil_Siege))]
+    [HarmonyPatch("CanBeBuilder")]
+    [HarmonyPatch(new Type[] { typeof(Pawn) })]
+    class Patch
+    {
+        static bool Prefix(Pawn p)
+        {
+            return (p.story != null);
+        }
+    }
+
+}
